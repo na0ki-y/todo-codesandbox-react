bacc78613ab38320896d6c6300b49ceecf5cc196
na <wehallo53.en@gmail.com>
2023-01-15 03:52:18 +0000

    Update App.jsx and Colofulmessage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
-import Colofulmessage from "./components/Colofulmessage";
-
+import React, { useState, useEffect } from "react";
+import { Colofulmessage } from "./components/Colofulmessage";
 const App = () => {
+  console.log("saisyo");
+  const [num, setNum] = useState(0); //分割 変数名と関数
+  const [faceflag, setfaceflag] = useState(true); //分割 変数名と関数
+
   const handleClick = () => {
     setNum(num + 1);
   };
-  const clickbutton = () => {
-    console.log("a");
-    setNum(1999); //State更新関数
+  const onoffclickbutton = () => {
+    setfaceflag(!faceflag);
   };
+  useEffect(() => {
+    console.log("配列を指定しないとベタ書きと同じ");
+  }); //(関数,配列)
+  useEffect(() => {
+    console.log("はじめだけ実行するにはからの配列");
+  }, []); //(関数,配列)
+  useEffect(() => {
+    console.log("配列にnumを入れる：numにだけ関心を向ける");
+  }, [num]); //(関数,配列)
+  useEffect(() => {
+    if (num % 3 == 0) {
+      console.log("a");
+      faceflag || setfaceflag(true); //falseのときだけにsetfaceflag
+    } else {
+      faceflag && setfaceflag(false); //trueのときだけにsetfaceflag
+    }
+  }, [num]); //(関数,配列)
+
   const contentState = { color: "blue", fontSize: "18px" }; //中にCSSの記法
   const contentpinkState = { color: "pink", fontSize: "18px" }; //中にCSSの記法
-  const [num, setNum] = useState(0); //分割 変数名と関数
+
   //JSXはJSのなかにhtmlかける。
   //複数なら外側で囲わないといけないから、<React.Fragment> </React.Fragment>or<></>
   //<!-- {}JSをかく {color:'red'}JSオブジェクトの書き方 -->
@@ -22,6 +42,8 @@ const App = () => {
       <Colofulmessage color="pink">げんき</Colofulmessage>
       <button onClick={handleClick}>カウントアップぼたん!</button>
       <p>{num}</p>
+      <button onClick={onoffclickbutton}>on/off</button>
+      {faceflag && <p>(TT)</p>}
     </>
   );
 };
--- src/components/Colofulmessage.jsx
+++ src/components/Colofulmessage.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
-const Colofulmessage = (props) => {
+export const Colofulmessage = (props) => {
   const { color, children } = props;
 
   const contentpinkState = { color: color, fontSize: "18px" }; //中にCSSの記法
   return <p style={contentpinkState}>{children}</p>;
 };
-export default Colofulmessage;
+//通常のとき typo防げる
+//export const Colofulmessage = (props) => {};
+////import は分割代入
+//import {Colofulmessage} from "./components/Colofulmessage";
+//
+//defaultのとき
+//const Colofulmessage = (props) => {};
+//export default Colofulmessage;
+////import は任意の名前
+//import Colofulmessage from "./components/Colofulmessage";
